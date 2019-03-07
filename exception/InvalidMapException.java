8338fbaabf0625449c24d503998f47b9c4211f9e
Hemil-s97 <48336718+Hemil-s97@users.noreply.github.com>
2019-03-07 18:44:32 -0500

    Create InvalidMapException.java
    Exception for an invalid map.

---
+++ exception/InvalidMapException.java
@@ -0,0 +1,16 @@
+package exception;
+
+/**
+ * Implementation of InvalidMapException exception.
+ */
+public class InvalidMapException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * This method is used print InvalidMapException message.
+	 * @param message contains exception message.
+	 */
+	public InvalidMapException(String message) {
+		super(message);
+	}
+}

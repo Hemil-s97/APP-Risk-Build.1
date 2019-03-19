37b459d9a1c5f8326511c53ac965f8c2dc76cb06
Hemil-s97 <48336718+Hemil-s97@users.noreply.github.com>
2019-03-19 18:18:40 -0400

    Update Context.java

--- strategy/Context.java
+++ strategy/Context.java
@@ -11,67 +11,80 @@ public class Context {
 	private Strategy strategy;
 
 	/**
-	 * method for strategy object
+	 * Strategy pattern for generation of reinforcement army
+	 * 
+	 * @param currentPlayer    name of current player
+	 * @param currentContinent name of current Continent
+	 * 
+	 */
+	public void executeReinforcementArmy(String currentPlayer, Continent currentContinent) {
+		strategy.generationOfReinforcementArmy(currentPlayer, currentContinent);
+	}
+
+	/**
+	 * Constructor to initialize strategy object
+	 * 
 	 * @param strategy pattern object
+	 * 
 	 */
 	public Context(Strategy strategy) {
 		this.strategy = strategy;
 	}
 
 	/**
-	 * Strategy pattern for reinforcement
-	 * @param currentPlayer name of current player
-	 * @param currentTerritory name of current territory
-	 * @param army no. of armies 
-	 * @param territory name of territory
-	 */
-	public void executeReinforcement(String currentPlayer, String currentTerritory, int army, Territories territory) {
-		strategy.doReinforcement(currentPlayer, currentTerritory, army, territory);
-	}
-
-	/**
-	 * Strategy pattern for reinforcement army generation
-	 * @param currentPlayer name of current player
-	 * @param currentContinent name of current Continent
-	 */
-	public void executeReinforcementArmy(String currentPlayer, Continent currentContinent) {
-		strategy.generateReinforcementArmy(currentPlayer, currentContinent);
-	}
-
-	/**
-	 * Strategy pattern for move army after attack
-	 * @param playerName name of player
-	 * @param currentTerritory name of current territory
+	 * Strategy pattern for fortification
+	 * 
 	 * @param fromTerritory name of territory
-	 * @param toTerritory name of territory
-	 * @param armies no. of armies
+	 * @param toTerritory   name of territory
+	 * @param territory     name of territory
+	 * @param getArmySelect no.of selected armies
+	 * 
 	 */
-	public void executeArmyAfterAttack(String playerName, Territories currentTerritory, String fromTerritory,
-			String toTerritory, int armies) {
-		strategy.moveArmyAfterAttack(playerName, currentTerritory, fromTerritory, toTerritory, armies);
+	public void executeFortification(String fromTerritory, String toTerritory, Territories territory,
+			int getArmySelect) {
+		strategy.performForitification(fromTerritory, toTerritory, territory, getArmySelect);
+	}
+
+	/**
+	 * Strategy pattern for reinforcement
+	 * 
+	 * @param territory        name of territory
+	 * @param currentTerritory name of current territory
+	 * @param army             no. of armies
+	 * @param currentPlayer    name of current player
+	 * 
+	 */
+	public void executeReinforcement(Territories territory, String currentTerritory, int army, String currentPlayer) {
+		strategy.performReinforcement(territory, currentTerritory, army, currentPlayer);
 	}
 
 	/**
 	 * Strategy pattern for attack
+	 * 
 	 * @param currentTerritory name of current territory
-	 * @param fromTerritory name of territory 
-	 * @param toTerritory name of territory
-	 * @param attackerDice no. of attacker's dices
-	 * @param defenderDice no. of defender's dices
+	 * @param fromTerritory    name of territory
+	 * @param attackerDice     no. of attacker's dices
+	 * @param toTerritory      name of territory
+	 * @param defenderDice     no. of defender's dices
+	 * 
 	 */
-	public void executeAttack(Territories currentTerritory, String fromTerritory, String toTerritory, int attackerDice,
+	public void executeAttack(Territories currentTerritory, String fromTerritory, int attackerDice, String toTerritory,
 			int defenderDice) {
-		strategy.doAttack(currentTerritory, fromTerritory, toTerritory, attackerDice, defenderDice);
+		strategy.performAttack(currentTerritory, fromTerritory, attackerDice, toTerritory, defenderDice);
 	}
 
 	/**
-	 * Strategy pattern for fortification
-	 * @param territory name of territory
-	 * @param fromTerritory name of territory
-	 * @param toTerritory name of territory
-	 * @param getArmySelect no.of selected armies
+	 * Strategy pattern for move army after attack
+	 * 
+	 * @param playerName       name of player
+	 * @param fromTerritory    name of territory
+	 * @param toTerritory      name of territory
+	 * @param currentTerritory name of current territory
+	 * @param armies           no. of armies
+	 * 
 	 */
-	public void executeFortification(Territories territory, String fromTerritory, String toTerritory, int getArmySelect) {
-		strategy.doForitification(territory, fromTerritory, toTerritory, getArmySelect);
+	public void executeArmyAfterAttack(String playerName, String fromTerritory, String toTerritory,
+			Territories currentTerritory, int armies) {
+		strategy.moveArmyAfterAttack(playerName, fromTerritory, toTerritory, currentTerritory, armies);
 	}
 }

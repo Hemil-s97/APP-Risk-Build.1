3b396732502c5b842bf0b2cfaeebf9a330024248
Hemil-s97 <48336718+Hemil-s97@users.noreply.github.com>
2019-03-21 12:12:22 -0400

    Create ControllerTestSuite.java

---
+++ TestController/ControllerTestSuite.java
@@ -0,0 +1,15 @@
+package TestController;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("TestController")
+
+/**
+ * Test suite class.
+ */
+public class ControllerTestSuite {
+
+}

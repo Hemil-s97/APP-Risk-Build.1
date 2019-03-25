1fa2bd7c9256338e80401a38d7a061eefc6ba986
narora25 <46978961+narora25@users.noreply.github.com>
2019-03-25 15:29:23 -0400

    Add files via upload

---
+++ RiskTestSuite.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages({"TestController","TestModel","TestValidation"})
+
+/**
+ * Test suite class.
+ */
+public class RiskTestSuite {
+
+}
